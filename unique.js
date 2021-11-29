2d485ed57c029f54a4c784bd69da30811b41661c
Marshood <marshood.ayoub@gmail.com>
2021-11-29 10:41:09 +0200

    make the keys uinque using hash map

--- unique.js
+++ unique.js
@@ -1,6 +1,7 @@
+let map = new Map();
 //123ad-asd12-16asd
 const APIKEY = (
-  length=16,
+  length = 16,
   ch = "-",
   div = 4,
   string = "ABCDEFGlaHgIhJKLjMNwOPqQRsSTcUVWnXYZm012u34f56789"
@@ -31,9 +32,15 @@ const APIKEY = (
   re = new RegExp(`.{${div}}`, "g");
   let chuncks = str.match(re);
   let new_value = chuncks.join(ch); //returns 123-456-789
-  return new_value;
+  if (map.has(new_value)) {
+    new_valu = APIKEY(length, ch, div, string);
+  } else {
+    map.set(new_value, "APIKEY");
+    return new_value;
+  }
+  return 'Error -> try again '
+
 };
-//asd
 //466322145452
 const RandomNum = (length = 16, string = "0123456789") => {
   if (isNaN(length) || isNaN(string)) {
@@ -49,43 +56,55 @@ const RandomNum = (length = 16, string = "0123456789") => {
       .join("");
   }
   let str = "";
-  i=0;
+  i = 0;
   while (i < length) {
     str += string.charAt(Math.floor(Math.random() * string.length));
     i++;
   }
-  return str;
+  if (map.has(new_value)) {
+    str = RandomNum(length, string);
+  } else {
+    map.set(str, "RandomNum");
+    return str;
+  }
+  return 'Error -> try again '
+
 };
 //jshAdfAfaFm
-const RandomString = (length = 16, string = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ") => {
-    if (isNaN(length) || !isNaN(string)) {
-      return "error";
-    }
-    let i = 0;
-    for (i = 0; i < 23; i++) {
-      string = string
-        .split("")
-        .sort(function () {
-          return 0.5 - Math.random();
-        })
-        .join("");
-    }
-    let str = "";
-    i=0;
+const RandomString = (
+  length = 16,
+  string = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+) => {
+  if (isNaN(length) || !isNaN(string)) {
+    return "error";
+  }
+  let i = 0;
+  for (i = 0; i < 23; i++) {
+    string = string
+      .split("")
+      .sort(function () {
+        return 0.5 - Math.random();
+      })
+      .join("");
+  }
+  let str = "";
+  i = 0;
 
-    while (i < length) {
-      str += string.charAt(Math.floor(Math.random() * string.length));
-      i++;
-    }
+  while (i < length) {
+    str += string.charAt(Math.floor(Math.random() * string.length));
+    i++;
+  }
+   if (map.has(new_value)) {
+    str = RandomString(length,string);
+  } else {
+    map.set(str, "RandomString");
     return str;
-  };
+  }
+  return 'Error -> try again '
+ };
 
-  
-// console.log(RandomNum(122, "123"))
-//  console.log(RandomString(122,"asd"));
-// console.log(APIKEY("13", "-"));
+ 
 
-
-module.exports.APIKEY=APIKEY;
-module.exports.RandomString=RandomString;
-module.exports.RandomNum=RandomNum;
+module.exports.APIKEY = APIKEY;
+module.exports.RandomString = RandomString;
+module.exports.RandomNum = RandomNum;
